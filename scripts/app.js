5a15902da379f0ed77f9cec8286e503535358eb0
Mohammad Razaul Islam <shovon231@gmail.com>
2022-03-12 22:36:41 -0500

    animation with the side bar

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,28 @@
-const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const navbarLinks = document.getElementsByClassName("navbar-links")[0];
-const navbar = document.getElementsByClassName("navbar")[0];
-toggleButton.addEventListener("click", () => {
-  navbar.classList.toggle("active");
-  navbarLinks.classList.toggle("active");
-});
+// const toggleButton = document.getElementsByClassName("toggle-button")[0];
+// const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+// toggleButton.addEventListener("click", () => {
+//   navbarLinks.classList.toggle("active");
+// });
+
+const navShow = () => {
+  const toggleButton = document.querySelector(".toggle-button");
+  const navbar = document.querySelector(".navbar-links");
+  const navbarLinks = document.querySelectorAll(".navbar-links li");
+
+  toggleButton.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+    navbarLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `linkShow 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
+    });
+    toggleButton.classList.toggle("toggleMenu");
+  });
+};
+
+navShow();
